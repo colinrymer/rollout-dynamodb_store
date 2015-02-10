242a569f5f19d881b33b454ba3f68ac844079474
Colin Rymer <colin.rymer@gmail.com>
2015-02-09 20:58:53 -0500

    initial commit

---
+++ lib/rollout/dynamodb_store.rb
@@ -0,0 +1,7 @@
+require "rollout/dynamodb_store/version"
+
+module Rollout
+  module DynamodbStore
+    # Your code goes here...
+  end
+end
---
+++ lib/rollout/dynamodb_store/version.rb
@@ -0,0 +1,5 @@
+module Rollout
+  module DynamodbStore
+    VERSION = "0.0.1"
+  end
+end
